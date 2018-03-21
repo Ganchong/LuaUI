127e775cb1451f44d42b8597da902af5976ddb4e
¶¡½Ü <1303040683@qq.com>
2018-03-21 16:40:45 +0800

    first push

---
+++ Assets/ToLua/Examples/01_HelloWorld/HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using LuaInterface;
+using System;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Awake()
+    {
+        LuaState lua = new LuaState();
+        lua.Start();
+        string hello =
+            @"   
+				UINames = {TestUI = 'TestUI',LoadingUI = 'LoadingUI',LoginUI = 'loginUI' }
+				name = UINames.LoginUI
+             
+                print('hello tolua#')
+
+				mtable = {'loginUI','mainUI'}
+
+				for i = 1,#mtable do 
+					if mtable[i] == name then print('equals index '..i) end
+				end	
+                                  
+            ";
+        
+        lua.DoString(hello, "HelloWorld.cs");
+        lua.CheckTop();
+        lua.Dispose();
+        lua = null;
+    }
+}
